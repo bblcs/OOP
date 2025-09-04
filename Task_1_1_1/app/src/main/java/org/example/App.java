d274f3a5b68ff57d2f49ec909936ec545dc2d1ca
evlc <dodgedodgedoggy@gmail.com>
2025-09-04 09:39:16 +0700

    App: use pq

--- Task_1_1_1/app/src/main/java/org/example/App.java
+++ Task_1_1_1/app/src/main/java/org/example/App.java
@@ -1,57 +1,39 @@
 package org.example;
 
+import java.util.PriorityQueue;
+
 public class App {
+    /**
+     * sorts an array using heapsort algorithm
+     *
+     * @param arr - array of integers
+     */
     static void sort(int arr[]) {
-        int n = arr.length;
-
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            sift_down(arr, n, i);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(arr[i]);
         }
 
-        for (int i = n - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-
-            sift_down(arr, i, 0);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = pq.poll();
         }
     }
 
-    // heapifies a subtree down from node i.
-    static void sift_down(int arr[], int n, int i) {
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-
-        if (l < n && arr[l] > arr[largest]) {
-            largest = l;
+    static void printArr(int arr[]) {
+        boolean first = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (!first) {
+                System.out.print(" ");
+            } else {
+                first = false;
+            }
+            System.out.print(arr[i]);
         }
-
-        if (r < n && arr[r] > arr[largest]) {
-            largest = r;
-        }
-
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            sift_down(arr, n, largest);
-        }
-    }
-
-    // utility
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) {
         int arr[] = {5, 4, 3, 2, 1};
         App.sort(arr);
-        App.printArray(arr);
+        App.printArr(arr);
     }
 }

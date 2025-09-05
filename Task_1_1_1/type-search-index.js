29a001d2082558a02eff20106bd2c615c35b59c2
bblcs <bblcs@users.noreply.github.com>
2025-09-05 06:44:41 +0000

    Deploying to gh-pages from @ bblcs/OOP@2c64b1745639d945501e2fc74ce1769ffc05fda9 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"App"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"App"},{"p":"org.example","l":"Heap"}];updateSearchResults();
